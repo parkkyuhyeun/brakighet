ee5985a12ee1f852fcad5e045486d2c066bc2683
gyuhyeun <ggm.s220079@ggh.goe.go.kr>
2022-10-27 23:37:02 +0900

    2022.10.27 23:36
    닫기 버튼
    버튼 눌리면 모든 버튼 비활성화
    닫기 버튼 누르면 다시 모든 버튼 활성화

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
     [SerializeField] GameObject popup;
     [SerializeField] GameObject otherPopup;
 
+    [SerializeField] Button pressedButton;
+    [SerializeField] Button anotherButton;
+
     [SerializeField] GameObject objectToCreate;
-    public void ChangePopupState()
+    public void OpenPopup()
     {
+        pressedButton.interactable = false;
+        anotherButton.interactable = false;
+
         if (otherPopup.gameObject.activeSelf == false)
         {
             popup.SetActive(!popup.gameObject.activeSelf);
@@ -20,5 +27,14 @@ public class ButtonManager : MonoBehaviour
         float randomX = Random.Range(-9f, 9f);
         float randomY = Random.Range(-2f, 2f);
         Instantiate(objectToCreate, new Vector3(randomX,randomY,0), Quaternion.identity);
+    }
+    public void ClosePopup()
+    {
+        if (popup.gameObject.activeSelf == true)
+        {
+            popup.SetActive(false);
+        }
+        pressedButton.interactable = true;
+        anotherButton.interactable = true;
     }
 }
